9621cb4dde0d0a4082ed7a1260ecb00bda2f5ebe
Arthur Gassner <arthur.gassner@gmail.com>
2012-09-19 21:02:12 +0100

    Tidied up a bit and added functionality to be able to donwload quotes from a symbol as an alternative

--- python/ftse.py
+++ python/ftse.py
@@ -26,6 +26,32 @@ def get_symbols(url):
 
     return symbols
 
+def format_date(date):
+    return datetime.strptime(date, "%Y-%m-%d").strftime("%Y%m%d")
+
+def transform_to_metastock(symbol, content):
+    # Date,Open,High,Low,Close,Volume,Adj Close
+    # 2012-08-17,583.00,592.00,581.00,582.50,2342000,582.50
+
+    # symbol,date,open,high,low,close,volume,trades
+    # ARM.L,20060706,112.5,112.5,108.75,109,20852214,0
+
+    lines = [ tuple(line.split(",")) for line in content.split("\n") ][1:-1]
+
+    return [ "%s,%s,%s,%s,%s,%s,%s,0" % (symbol, format_date(line[0]), line[1], line[2], line[3], line[4], line[5]) for line in lines ]
+
+def get_quotes(symbol):
+    t = tm.time()
+
+    content = get_httplib2("http://ichart.yahoo.com/table.csv?s=%s" % symbol)
+
+    if content:
+        file = open("%s/%s.txt" % (data_dir, symbol), "w")
+        [file.write("%s\n" % line) for line in transform_to_metastock(symbol, content)]
+        file.close()
+
+    print "Got %s in %.3f" % (symbol, tm.time()-t)
+
 def get_constituents():
     
     t = tm.time()
@@ -50,31 +76,9 @@ def get_constituents():
     print "Found %s symbols" % len(symbols)
     print "Took %.3fs" % (tm.time()-t)
 
-def get_quotes(symbol):
-    t = tm.time()
-
-    content = get_httplib2("http://ichart.yahoo.com/table.csv?s=%s" % symbol)
-
-    if content:
-        file = open("%s/%s.txt" % (data_dir, symbol), "w")
-        [file.write("%s\n" % line) for line in transform_to_metastock(symbol, content)]
-        file.close()
-
-    print "Got %s in %.3f" % (symbol, tm.time()-t)
-
-def transform_to_metastock(symbol, content):
-    # Date,Open,High,Low,Close,Volume,Adj Close
-    # 2012-08-17,583.00,592.00,581.00,582.50,2342000,582.50
-
-    # symbol,date,open,high,low,close,volume,trades
-    # ARM.L,20060706,112.5,112.5,108.75,109,20852214,0
-
-    lines = [ tuple(line.split(",")) for line in content.split("\n") ][1:-1]
-
-    return [ "%s,%s,%s,%s,%s,%s,%s,0" % (symbol, format_date(line[0]), line[1], line[2], line[3], line[4], line[5]) for line in lines ]
-
-def format_date(date):
-    return datetime.strptime(date, "%Y-%m-%d").strftime("%Y%m%d")
-
 if __name__ == "__main__":
-    get_constituents()
+    if len(sys.argv) > 1:
+        for symbol in sys.argv[1:]:
+            get_quotes(symbol)
+    else:
+        get_constituents()
